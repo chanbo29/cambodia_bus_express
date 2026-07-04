88183177ba8aff3d3e0100ad9ce66d317899d690
Chheng Chanbo <chanbochheng4@gmail.com>
2026-07-05 00:04:00 +0700

    3D rotating carousel for News & Promotions

--- frontend/src/components/NewsPromotions.jsx
+++ frontend/src/components/NewsPromotions.jsx
@@ -5,52 +5,52 @@ const PROMO_IMAGES = [
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921733/688304995_122168342240930524_871956963636454236_n_rq1vqf.jpg",
     title: "Happy Father's Day",
-    tag: "Father's Day",
+    tag: "Welcome",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921733/726868848_2919129251771109_229754996216978784_n_gn82ej.jpg",
     title: "Siem Reap Express Now Open",
-    tag: "New Route",
+    tag: "To",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921732/707843890_122171604482930524_6553122813415326891_n_vwhnm8.jpg",
     title: "Sihanoukville Weekend Getaway",
-    tag: "Promo",
+    tag: "Cambodia",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921732/705387882_122170578674930524_2916474511808824980_n_t34sd1.jpg",
     title: "Book Early, Save More",
-    tag: "Save 20%",
+    tag: "Bus",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921732/704467899_122170329038930524_5024689999608613795_n_ujyrbx.jpg",
     title: "",
-    tag: "",
+    tag: "Express",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782921731/696734506_122168990546930524_7695005599975160121_n_hpcmd2.jpg",
     title: "",
-    tag: "",
+    tag: "Book",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782924151/658133175_122160127694930524_6280922826816495072_n_oess68.jpg",
     title: "",
-    tag: "",
+    tag: "Now",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782924151/672687353_122162782268930524_6565412347807004389_n_lfcoi9.jpg",
     title: "",
-    tag: "",
+    tag: "And",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782924151/632206418_122153741198930524_5139780497092700137_n_t2ldu6.jpg",
     title: "",
-    tag: "",
+    tag: "Travel",
   },
   {
     src: "https://res.cloudinary.com/jvwlddbl/image/upload/v1782924151/651009201_122157313316930524_5414813989880167356_n_ofaebq.jpg",
     title: "",
-    tag: "",
+    tag: "Now",
   },
 ];
 
